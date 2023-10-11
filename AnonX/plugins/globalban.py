8a961ced48c1c15bc752fa61266c05e453597d30
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-11 14:29:58 +0300

    Update globalban.py

--- AnonX/plugins/globalban.py
+++ AnonX/plugins/globalban.py
@@ -63,11 +63,10 @@ async def gbanuser(client, message: Message, _):
             await app.ban_chat_member(chat_id, user_id)
             number_of_chats += 1
         except FloodWait as e:
-            await asyncio.sleep(int(e.x))
+            await asyncio.sleep(int(e.value))
         except Exception:
             pass
     await add_banned_user(user_id)
-    await message.reply_sticker("CAACAgUAAxkBAAIjZmKPbsuJzL3TVFQ7q2lc_rRuqa6xAAIyCQACHjuBVOwXUJB64QeSJAQ")
     await message.reply_text(
         _["gban_6"].format(mention, number_of_chats)
     )
@@ -107,11 +106,10 @@ async def gungabn(client, message: Message, _):
             await app.unban_chat_member(chat_id, user_id)
             number_of_chats += 1
         except FloodWait as e:
-            await asyncio.sleep(int(e.x))
+            await asyncio.sleep(int(e.value))
         except Exception:
             pass
     await remove_banned_user(user_id)
-    await message.reply_sticker("CAACAgUAAxkBAAIjbGKPb3oOFXIT3KSxlIoefG7jTLOiAAJuBgAC_415VDBZlDYZrGxCJAQ")
     await message.reply_text(
         _["gban_9"].format(mention, number_of_chats)
     )
@@ -125,7 +123,7 @@ async def gbanned_list(client, message: Message, _):
     if counts == 0:
         return await message.reply_text(_["gban_10"])
     mystic = await message.reply_text(_["gban_11"])
-    msg = "ɢʙᴀɴɴᴇᴅ ᴜsᴇʀs:\n\n"
+    msg = "Gbanned Users:\n\n"
     count = 0
     users = await get_banned_users()
     for user_id in users:
@@ -137,7 +135,7 @@ async def gbanned_list(client, message: Message, _):
             )
             msg += f"{count}➤ {user}\n"
         except Exception:
-            msg += f"{count}➤ [ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ]{user_id}\n"
+            msg += f"{count}➤ [Unfetched User]{user_id}\n"
             continue
     if count == 0:
         return await mystic.edit_text(_["gban_10"])
