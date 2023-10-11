a14a5256737801422c7ad619953c83de414cc8db
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-11 11:08:10 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -3,57 +3,60 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import config
-from config import SUPPORT_CHANNEL
 
 
-
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def start_pannel(_, BOT_USERNAME):
     buttons = [
         [
             InlineKeyboardButton(
-                text="‹ زیادم بکە بۆ گرووپەکەت ›",
+                text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="❈ فەرمانەکان ❈",
+                text=_["S_B_8"],
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="❈ یارمەتی ❈",
-                callback_data="settings_helper"
+                text=_["S_B_2"], callback_data="settings_helper"
             ),
         ],
-     ]
+    ]
     return buttons
 
 
+# noinspection PyTypeChecker
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="‹ زیادم بکە بۆ گرووپەکەت ›",
+                text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="❈ فەرمانەکان ❈", callback_data="settings_back_helper"
+                text=_["S_B_8"], callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="❈ کڕینی بۆت ❈", url=f"https://t.me/IQ7amo"
+                text=_["S_B_4"], url=f"https://t.me/MGIMT"
             ),
             InlineKeyboardButton(
-                text="✪ خاوەنی بۆت ✪", user_id=OWNER
+                text=_["S_B_3"], url=f"https://t.me/IQSUPP"
             )
         ],
         [
             InlineKeyboardButton(
-                text="✬ کەناڵی بۆت ✬", url=f"https://t.me/MGIMT"
+                text=_["S_B_7"], user_id=OWNER
+            )
+
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG"
             )
         ],
-     ]
+    ]
     return buttons
