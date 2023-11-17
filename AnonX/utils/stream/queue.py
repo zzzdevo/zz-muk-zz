5311b492b21ef09f2f334cc1eb17b1863183b245
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-17 11:22:44 +0300

    Update queue.py

--- AnonX/utils/stream/queue.py
+++ AnonX/utils/stream/queue.py
@@ -30,13 +30,11 @@ async def put_queue(
         "chat_id": original_chat_id,
         "file": file,
         "vidid": vidid,
-        "user_id": user_id,
         "seconds": duration_in_seconds,
         "played": 0,
     }
     if forceplay:
-        check = db.get(chat_id)
-        if check:
+        if check := db.get(chat_id):
             check.insert(0, put)
         else:
             db[chat_id] = []
@@ -78,8 +76,7 @@ async def put_queue_index(
         "played": 0,
     }
     if forceplay:
-        check = db.get(chat_id)
-        if check:
+        if check := db.get(chat_id):
             check.insert(0, put)
         else:
             db[chat_id] = []
