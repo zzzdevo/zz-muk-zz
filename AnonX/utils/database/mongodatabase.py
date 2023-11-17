e3ed6e79ec39c560154271bb8e795a8e27425a23
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-17 10:30:43 +0300

    Update mongodatabase.py

--- AnonX/utils/database/mongodatabase.py
+++ AnonX/utils/database/mongodatabase.py
@@ -1,16 +1,6 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from typing import Dict, List, Union
 
-from YukkiMusic.core.mongo import mongodb
+from AnonX.core.mongo import mongodb
 
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
