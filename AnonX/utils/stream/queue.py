1a4eb4ce9755f77c1cc70cd3098c3aed6ed19bca
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-17 10:35:15 +0300

    Update queue.py

--- AnonX/utils/stream/queue.py
+++ AnonX/utils/stream/queue.py
@@ -1,18 +1,8 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from YukkiMusic.misc import db
+from AnonX.misc import db
 
 
 async def put_queue(
