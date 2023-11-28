0c4cbb6b7650cc5c9bfa62c0b31c1238143de5af
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-29 01:03:14 +0300

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -23,7 +23,7 @@ class YukkiBot(Client):
     def __init__(self):
         LOGGER(__name__).info("Starting Bot")
         super().__init__(
-            "YukkiMusicBot",
+            "AnonMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
