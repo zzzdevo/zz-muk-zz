e21f8bd9e9d95ec6918210adc0bd0d10c7679233
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-29 05:58:11 +0300

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 import sys
 
 from pyrogram import Client
@@ -11,7 +21,7 @@ from ..logging import LOGGER
 
 class AnonXBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info("Starting Bot")
         super().__init__(
             "AnonXMusic",
             api_id=config.API_ID,
@@ -50,8 +60,6 @@ class AnonXBot(Client):
                     )
             except:
                 pass
-        else:
-            pass
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
@@ -59,7 +67,7 @@ class AnonXBot(Client):
             )
             sys.exit()
         if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
+            self.name = f"{get_me.first_name} {get_me.last_name}"
         else:
             self.name = get_me.first_name
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
